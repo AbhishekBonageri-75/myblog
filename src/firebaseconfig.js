45bbbadcdcd293a908295c31885d4cd6266f51b7
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-03-26 11:54:57 -0500

    firebase config file added

---
+++ src/firebaseconfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCi4JbwBd0ZDXRj0YJhDeVah5-Jjfo-1Ec",
+  authDomain: "blog-c28ba.firebaseapp.com",
+  projectId: "blog-c28ba",
+  storageBucket: "blog-c28ba.appspot.com",
+  messagingSenderId: "5699661866",
+  appId: "1:5699661866:web:f379dd91c92f41bc831353",
+  measurementId: "G-1S7BH8F4DX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
