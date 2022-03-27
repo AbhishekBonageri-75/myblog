feee2c39f9a336e91eddaa59a22f543e6824c8d7
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-03-27 00:23:52 -0500

    useState hook in action.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { useState } from "react";
+
 const Home = () => {
 
-    function sayHello(name) {
-        alert("Hello "+name);
+    const [name , setName] = useState("Abhishek");
+    const [age , setAge] = useState("21");
+
+    function sayHello() {
+        setName('Pachhu');
+        setAge(25);
     }
 
 
     return ( 
         <div className="homeContent">
             <h2> This is home page</h2>
-            <button  onClick={() =>sayHello("Abhishek")}>Click me</button> {/* passing the data to another function on button click  */}
-            
+            <p>{name} is {age}</p>
+            <button  onClick={sayHello}>Click me</button> {/* passing the data to another function on button click  */}
         </div>
      );
 }
