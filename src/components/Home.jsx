941c29af3f8f80f54b94f2f990d190dcc85d5605
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-03-27 02:14:54 -0500

    designed blog content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,36 @@ import React from "react";
 import { useState } from "react";
 
 const Home = () => {
+    const [title , setTitle] = useState();
+    const [author , setAuthor] = useState();
+    const [body , setBody] = useState();
+    
+    const [blogs,setBlogs] = useState([
+        {title: "Mobile Development",body:"Blog content ... 1" , author: "Abhishek",id:1},
+        {title: "SpringBoot Applications",body:"Blog content ... 2" , author: "Prasad",id:2},
+        {title: "Web Development",body:"Blog content ... 3" , author: "Ravi",id:3}
+    ]);
 
-    const [name , setName] = useState("Abhishek");
-    const [age , setAge] = useState("21");
-
-    function sayHello() {
-        setName('Pachhu');
-        setAge(25);
-    }
-
+   
 
     return ( 
         <div className="homeContent">
-            <h2> This is home page</h2>
-            <p>{name} is {age}</p>
-            <button  onClick={sayHello}>Click me</button> {/* passing the data to another function on button click  */}
+            {blogs.map((blog)=>(
+                <div className="fulldiv">
+                    <div className="preview" key={blog.id}> 
+                        <h2>{blog.title}</h2>
+                        <h5>Author : {blog.author}</h5>
+                        <p>{blog.body}</p>    
+                    </div>
+                    <div className="options">
+                        <ul>
+                            <li>Delete</li>
+                            <li>Edit</li>
+                
+                        </ul>
+                    </div>
+                </div>
+            ))}
         </div>
      );
 }
