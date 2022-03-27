1eee2cae42d8193afbbbdeee752f03d0a889342d
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-03-27 03:47:09 -0500

    fetching data from json-server

--- src/components/Bloglist.jsx
+++ src/components/Bloglist.jsx
@@ -8,7 +8,7 @@ const Bloglist = ({blogs , title , handleDelete}) => {
                     <div className="preview" key={blog.id}> 
                         <h2>{blog.title}</h2>
                         <h5>Author : {blog.author}</h5>
-                        <p>{blog.body}</p>    
+                        {/* <p>{blog.body}</p>     */}
                     </div>
                     <div className="options">
                         <ul>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,7 @@ import Bloglist from "./Bloglist";
 const Home = () => {
    
     
-    const [blogs,setBlogs] = useState([
-        {title: "Mobile Development",body:"Blog content ... 1" , author: "Abhishek",id:1},
-        {title: "SpringBoot Applications",body:"Blog content ... 2" , author: "Prasad",id:2},
-        {title: "Web Development",body:"Blog content ... 3" , author: "Ravi",id:3}
-    ]);
+    const [blogs,setBlogs] = useState(null);
 
 
     const handleDelete = (id) => {
@@ -18,12 +14,18 @@ const Home = () => {
     }
 
    useEffect(() =>{
-       console.log("Rendered"); // this will be called everytime the component is rendered
-   })
+       fetch('http://localhost:8000/blogs')
+       .then(res=>{
+           return res.json();
+       })
+       .then(data=>{
+            setBlogs(data);
+       })
+   },[])
 
     return ( 
         <div className="homeContent">
-           <Bloglist blogs={blogs} title={"All Blogs"} handleDelete={handleDelete}/>
+          {blogs && <Bloglist blogs={blogs} title={"All Blogs"} handleDelete={handleDelete}/>}
         </div>
      );
 }
